bedee96ee20e8b603832d27aaecf314422d52402
Ankon Costa <ankoncosta247@gmail.co>
2022-10-11 01:03:34 +0600

    added hero section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layouts/Main';
+import Home from './components/Home/Home';
+import Topics from './components/Topics/Topics';
+import Blog from './components/Blog/Blog';
+import Four404Four from './components/Four404Four/Four404Four';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/topics',
+          element: <Topics></Topics>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        }
+      ]
+    },
+    {
+      path: '/*',
+      element: <Four404Four></Four404Four>
+    }
+  ])
+
   return (
     <div className="App">
-
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => {
+    return (
+        <div className='w-10/12 my-12 mx-auto'>
+            <div className='border border-gray-100 text-3xl py-6 text-white text-bold bg-blue-400'>
+                <h2>Have Your Skill Test </h2>
+            </div>
+        </div>
+    );
+};
+
+export default Home;
